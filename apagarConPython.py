66433d9720a92b52a863682f2070bbe86cc99ec0
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-11-08 19:37:49 -0300

    programo funciones en Python

--- apagarConPython.py
+++ apagarConPython.py
@@ -1,15 +1,26 @@
 
+# coding=utf-8
+
 import subprocess
+import datetime
 
-hora = input("Ingresar hora (valido entre 00 y 23): ");
-minuto = input("Ingresar minuto (valido entre 00 y 59): ");
+hh = input("Ingresar hora (valido entre 0 y 23): ");
+mm = input("Ingresar minuto (valido entre 0 y 59): ");
 
-def Validar(hora = None, minuto = None):
-    # return True
-    print(str(hora) + ':' + str(minuto))
+def Shutdown():  
+    while True:
+        if (datetime.datetime.now().hour == int(hh)) and (datetime.datetime.now().minute == int(mm)):
+            break
+        pass
+    subprocess.call("poweroff", shell=True)
+
+# validaciones:    > <
+try:
+    if (int(hh) >= 0 and int(hh) < 24) and (int(mm) >= 0 and int(mm) < 60):
+        Shutdown()
+except:
+    print("Error: No se pudo realizar la conversión de datos ingresados")
     pass
 
-Validar(hora, minuto);
 
-
-subprocess.call("ls -la ", shell=True)
+#subprocess.call("ls -la ", shell=True)
