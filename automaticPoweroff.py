b0604048c0ae95356965d43444adddc709656ba4
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-02-26 00:06:03 -0300

    actualizo código de automaticPoweroff

--- automaticPoweroff.py
+++ automaticPoweroff.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 import subprocess, datetime
-
-text_answer = "Ingresar hora válida para realizar la acción (ej: 14:30): "
+debug_mode = False
 
 def main():
     user_input = input("Ingrese (1) para apagar, o (2) para reiniciar: ")
     try:
         option = int(user_input)
         if option not in (1, 2): raise Exception("Error: debe ingresar una opción válida!")
+        actual_time = get_actual_time()
         action_time = ask_time_to_action()
 
     except ValueError as e:
@@ -17,26 +17,30 @@ def main():
     except Exception as e:
         print(e)
     else:
-        actual_time = get_actual_time()
-        action_time = ask_time_to_action()
         # Optimizar esto (cambiar el while True):
+        action_name = "apagará" if option == 1 else "reiniciará"
+        print("El sistema se {} a las {}".format(action_name, action_time))
+        print("Presione Ctrl + Z para detener.")
+
         while True:
             if (actual_time == action_time) and option == 1: poweroff()
             elif (actual_time == action_time) and option == 2: reboot()
 
 
 def poweroff():
-    subprocess.call("poweroff", shell=True)
+    if debug_mode: print("Apagando...")
+    else: subprocess.call("poweroff", shell=True)
 
 def reboot():
-    subprocess.call("reboot", shell=True)
+    if debug_mode: print("Reiniciando...")
+    else: subprocess.call("reboot", shell=True)
 
 def get_actual_time():
     actual_time = datetime.datetime.now()
     return str(actual_time.hour) + ":" + str(actual_time.minute)
 
 def ask_time_to_action():
-    user_date = input(text_answer)
+    user_date = input("Ingresar hora válida para realizar la acción (ej: 14:30): ")
     try:
         hh = int(user_date.split(":")[0])
         mm = int(user_date.split(":")[1])
@@ -46,7 +50,11 @@ def ask_time_to_action():
         raise e
     else:
         if hh < 10: hh = ("0" + str(hh))
+        else: hh = str(hh)
+
         if mm < 10: mm = ("0" + str(mm))
+        else: mm = str(mm)
+
         return hh + ":" + mm
 
 if __name__ == "__main__":
